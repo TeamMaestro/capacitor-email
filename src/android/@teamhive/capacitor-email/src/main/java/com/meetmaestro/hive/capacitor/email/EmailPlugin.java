6999cc8cf7f37a226404bb4294e74277706e8daa
Osei Fortune <fortune.osei@yahoo.com>
2018-09-27 13:34:17 -0400

    Android support

--- src/android/@teamhive/capacitor-email/src/main/java/com/meetmaestro/hive/capacitor/email/EmailPlugin.java
+++ src/android/@teamhive/capacitor-email/src/main/java/com/meetmaestro/hive/capacitor/email/EmailPlugin.java
@@ -1,26 +1,30 @@
 package com.meetmaestro.hive.capacitor.email;
 
+import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.text.Html;
+import android.util.Log;
+import android.util.Patterns;
 
-import com.getcapacitor.JSArray;
-import com.getcapacitor.JSObject;
-import com.getcapacitor.NativePlugin;
-import com.getcapacitor.Plugin;
-import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
+import com.getcapacitor.*;
+import com.google.android.gms.common.AccountPicker;
 
 import org.json.JSONException;
 
-import android.os.Build;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @NativePlugin(
-        requestCodes = EmailPlugin.REQUEST_CODE
+        requestCodes = {EmailPlugin.REQUEST_CODE}
 )
 public class EmailPlugin extends Plugin {
     private JSObject aliases = new JSObject();
@@ -72,11 +76,12 @@ public class EmailPlugin extends Plugin {
         String subject = call.getString("subject", "");
         String body = call.getString("body", "");
         boolean isHtml = call.getBoolean("isHtml", true);
-        String type = call.getString("type");
-        String app = call.getString("app", "mailto");
+        String type = call.getString("type", defaults.getString("type"));
+        String app = call.getString("app", "mailto://");
         String chooserHeader = call.getString("Open with", "Open with");
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType(type);
+        ArrayList<Uri> uris = new ArrayList<Uri>();
         if (aliases.has(app)) {
             PackageManager pm = getActivity().getPackageManager();
             try {
@@ -88,6 +93,28 @@ public class EmailPlugin extends Plugin {
 
             }
         }
+
+        try {
+            List<String> list = attachments.toList();
+            for (String attachment : list) {
+                if (attachment.startsWith("_capacitor_")) {
+                    attachment = attachment.replace("_capacitor_", "file://");
+                }
+                Uri uri = Uri.parse(attachment);
+                if (uri != null && uri != Uri.EMPTY) uris.add(uri);
+            }
+        } catch (JSONException ignored) {
+
+        }
+
+        if (uris.size() > 1) {
+            intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.putExtra(Intent.EXTRA_STREAM, uris);
+        } else if (uris.size() == 1) {
+            intent.putExtra(Intent.EXTRA_STREAM, uris.get(0));
+        }
+
         intent.putExtra(Intent.EXTRA_EMAIL, getArray(to));
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
         intent.putExtra(Intent.EXTRA_BCC, getArray(bcc));
@@ -102,6 +129,7 @@ public class EmailPlugin extends Plugin {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         getActivity().startActivity(Intent.createChooser(intent, chooserHeader));
+        call.success();
 
     }
 
@@ -115,6 +143,7 @@ public class EmailPlugin extends Plugin {
                     call.reject("");
                 }
             }
+            call.resolve();
         } else {
             call.resolve();
         }
@@ -123,18 +152,39 @@ public class EmailPlugin extends Plugin {
 
     @PluginMethod()
     public void isAvailable(PluginCall call) {
-        String app = call.getString("app", "");
+        String app = call.getString("alias", "");
+        boolean has = checkPermission();
+        JSObject object = new JSObject();
         PackageManager pm = getActivity().getPackageManager();
         if (aliases.has(app)) {
             app = aliases.getString(app);
         }
-        try {
-            ApplicationInfo info = pm.getApplicationInfo(aliases.getString(app), 0);
-            if (info.enabled) {
-                call.success();
+        object.put("hasAccount", false);
+
+        if (has) {
+            AccountManager am;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                am = AccountManager.get(getContext());
+            } else {
+                am = AccountManager.get(getContext());
             }
+
+            Pattern emailPattern = Patterns.EMAIL_ADDRESS;
+            for (Account account : am.getAccounts()) {
+                if (emailPattern.matcher(account.name).matches()) {
+                    object.put("hasAccount", true);
+                }
+            }
+
+        }
+
+        try {
+            ApplicationInfo info = pm.getApplicationInfo(app, 0);
+            object.put("hasApp", true);
         } catch (PackageManager.NameNotFoundException e) {
-            call.success();
+            object.put("hasApp", false);
+        } finally {
+            call.success(object);
         }
 
     }
@@ -144,17 +194,35 @@ public class EmailPlugin extends Plugin {
         open(call);
     }
 
+    private void requestAccountPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.GET_ACCOUNTS}, REQUEST_CODE);
+    }
+
     @PluginMethod()
     public void requestPermission(PluginCall call) {
-        ActivityCompat.requestPermissions(getActivity(), new String[]{"android.permission.GET_ACCOUNTS"}, REQUEST_CODE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                    null, false, null, null, null, null);
+            startActivityForResult(call, intent, REQUEST_CODE);
+        } else {
+            requestAccountPermission();
+        }
+    }
+
+    private boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            AccountManager am = AccountManager.get(getContext());
+            return am.getAccounts().length > 0;
+        }
+        return (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED);
     }
 
     @PluginMethod()
     public void hasPermission(PluginCall call) {
-        boolean has = (ActivityCompat.checkSelfPermission(getContext(), "android.permission.GET_ACCOUNTS") == PackageManager.PERMISSION_GRANTED);
-        JSObject object = new JSObject();
-        object.put("value", has);
-        call.resolve(object);
+        if (checkPermission()) {
+            call.resolve();
+        }
+        call.reject("");
     }
 
     @PluginMethod()
