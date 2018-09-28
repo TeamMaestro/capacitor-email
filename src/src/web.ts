6c07d4cf65de6f4e3966ec8d55a739c8fbe1d41a
Osei Fortune <fortune.osei@yahoo.com>
2018-09-28 14:09:54 -0400

    Fix web

--- src/src/web.ts
+++ src/src/web.ts
@@ -1,83 +1,79 @@
-import { WebPlugin, mergeWebPlugin } from '@capacitor/core';
-import {
-  IEmail,
-  AvailableOptions,
-  AvailableResults,
-  EmailOptions
-} from './definitions';
-import { Plugins } from '@capacitor/core';
+import { mergeWebPlugin, Plugins, WebPlugin } from '@capacitor/core';
+import { AvailableOptions, AvailableResults, EmailOptions, IEmail } from './definitions';
+
 const mailto = 'mailto:';
 const defaults = {
-  app: mailto,
-  subject: '',
-  body: '',
-  to: [] as any[],
-  cc: [] as any[],
-  bcc: [] as any[],
-  attachments: [] as any[],
-  isHtml: true,
-  chooserHeader: 'Open with'
+    app: mailto,
+    subject: '',
+    body: '',
+    to: [] as any[],
+    cc: [] as any[],
+    bcc: [] as any[],
+    attachments: [] as any[],
+    isHtml: true,
+    chooserHeader: 'Open with'
 };
+
 export class EmailPluginWeb extends WebPlugin implements IEmail {
-  isAvailable(options?: AvailableOptions): Promise<AvailableResults> {
-    return Promise.resolve({
-      hasAccount: false,
-      hasApp: false
-    });
-  }
-
-  open(options?: EmailOptions | null): Promise<{}> {
-    let url = 'mailto:';
-    if (!options) {
-      window.open(url);
-      return Promise.resolve({});
+    isAvailable(options?: AvailableOptions): Promise<AvailableResults> {
+        return Promise.resolve({
+            hasAccount: false,
+            hasApp: false
+        });
     }
-    const keys = Object.keys(options);
-    for (let key of keys) {
-      let value;
-      if (key === 'to') {
-        url = url + `${((options as any)[key] as string[]).join(';')}?`;
-      } else if (key === 'cc' || key === 'bcc') {
-        value = ((options as any)[key] as string[]).join(';');
-        url = url + `${key}=${value}&`;
-      } else {
-        value = (options as any)[key];
-        url = url + `${key}=${value}&`;
-      }
+
+    open(options?: EmailOptions | null): Promise<any> {
+        let url = 'mailto:';
+        if (!options) {
+            window.open(url);
+            return Promise.resolve({});
+        }
+        const keys = Object.keys(options);
+        for (let key of keys) {
+            let value;
+            if (key === 'to') {
+                url = url + `${((options as any)[key] as string[]).join(';')}?`;
+            } else if (key === 'cc' || key === 'bcc') {
+                value = ((options as any)[key] as string[]).join(';');
+                url = url + `${key}=${value}&`;
+            } else {
+                value = (options as any)[key];
+                url = url + `${key}=${value}&`;
+            }
+        }
+        window.open(url);
+        return Promise.resolve({});
     }
-    window.open(url);
-    return Promise.resolve({});
-  }
 
-  openDraft(): Promise<{}> {
-    return this.open();
-  }
+    openDraft(): Promise<any> {
+        return this.open();
+    }
 
-  requestPermission(): Promise<{}> {
-    return Promise.resolve({});
-  }
+    requestPermission(): Promise<any> {
+        return Promise.resolve({});
+    }
 
-  hasPermission(): Promise<{ value: boolean }> {
-    return Promise.resolve({ value: true });
-  }
+    hasPermission(): Promise<any> {
+        return Promise.resolve();
+    }
 
-  getDefaults(): Promise<{}> {
-    return Promise.resolve(defaults);
-  }
+    getDefaults(): Promise<any> {
+        return Promise.resolve(defaults);
+    }
 
-  getAliases(): Promise<{}> {
-    return Promise.resolve({});
-  }
+    getAliases(): Promise<any> {
+        return Promise.resolve({});
+    }
 
-  constructor() {
-    super({
-      name: 'EmailPlugin',
-      platforms: ['web']
-    });
-  }
+    constructor() {
+        super({
+            name: 'EmailPlugin',
+            platforms: ['web']
+        });
+    }
 }
 
 const EmailWeb = new EmailPluginWeb();
 
 export { EmailWeb };
-mergeWebPlugin(Plugins,EmailWeb);
+mergeWebPlugin(Plugins, EmailWeb);
